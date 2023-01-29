f1d7b6db5eaa01435de634bb4d1c4e45879bd85d
moseyarthas <moseyarthas@gmail.com>
2023-01-30 00:19:21 +0300

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 1, b = 1;
+        double ans = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(ans);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.println((char)99);
+        System.out.println((char) 104);
+        System.out.println((char)97);
+        System.out.println((char)114);
     }
 }
